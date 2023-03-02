bde4873b5013b3f10923cf4513dfefff5517a758
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2023-03-02 08:51:51 +0700

    fix radio status
    Radio Status fix client ( not include js)

--- client/main.lua
+++ client/main.lua
@@ -291,6 +291,8 @@ AddEventHandler('pma-voice:setTalkingMode', function(newTalkingRange)
 end)
 
 -- TALKING ACTIVE
+
+local usingradio = false
 AddEventHandler("pma-voice:radioActive", function(radioTalking) -- i thin this is the final result but i didn't testing yet pog!!!
 		usingRadio = radioTalking 
         SendNUIMessage({
@@ -305,13 +307,13 @@ local usingRadio = false
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(250)
-	usingRadio = false
+	if not usingradio then
         local playerTalking = NetworkIsPlayerTalking(PlayerId())
-print("talking active 250 milisecond")
         SendNUIMessage({
             action = 'talking',
             IsTalking = playerTalking
         })
+				end
     end
 end)
 
